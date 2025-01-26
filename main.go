5b7b3c2b504305444d2d415f85af477469d35ae1
Siddhartha Tiwari <siddharthatiwari37@gmail.com>
2025-01-26 22:35:49 +0545

    Help command added

--- main.go
+++ main.go
@@ -1,12 +1,74 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
 	works := Works{}
 	storage := NewStorage[Works]("works.json")
 	storage.Load(&works)
 
+	if len(os.Args) < 2 || (len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "-help")) {
+		displayHelpMessage()
+		os.Exit(0)
+	}
+
 	cmdFlags := NewCmdFlags()
 	cmdFlags.Execute(&works)
 
 	storage.Save(works)
+}
+
+func displayHelpMessage() {
+	logo := `
+ _____        _             
+|_   _|      | |            
+  | | ___ ___| | _____ _ __ 
+  | |/ __/ __| |/ / _ \ '__|
+ _| |\__ \__ \   <  __/ |   
+ \___/___/___/_|\_\___|_|   
+`
+
+	helpText := fmt.Sprintf(`
+%s
+Tasker CLI - Efficient Task Management
+=====================================================
+
+Commands:
+  -add         Add a new task
+               Example: tasker -add "Complete project report"
+
+  -edit-work   Edit a task's title
+               Format: tasker -edit-work "id:new title"
+               Example: tasker -edit-work "2:Updated task description"
+
+  -change-branch Modify task's branch assignment
+                 Format: tasker -change-branch "id:new branch"
+                 Example: tasker -change-branch "1:Development"
+
+  -assign      Assign a branch to a specific task
+               Format: tasker -assign "id:branch_name"
+               Example: tasker -assign "3:Backend"
+
+  -del         Delete a task by index
+               Example: tasker -del 2
+
+  -toggle      Toggle task completion status
+               Example: tasker -toggle 1
+
+  -list        Display all tasks
+               Example: tasker -list
+
+Global Options:
+  -h, -help    Show this help message
+  -version     Display application version
+
+For more information, visit:
+https://github.com/Sidontheway/Tasker_CLI
+`, logo)
+
+	fmt.Println(strings.TrimSpace(helpText))
 }
