172aa80aaa4ade8e28c37297d778a159b64b703e
Siddhartha Tiwari <siddharthatiwari37@gmail.com>
2025-01-26 23:00:03 +0545

    Change from flag to sub commands

--- command.go
+++ command.go
@@ -1,100 +1,109 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type CmdFlags struct {
-	Add          string
-	Del          int
-	EditWork     string
-	ChangeBranch string
-	AssignBranch string
-	Toggle       int
-	List         bool
-}
+// type CmdFlags struct {
+// 	Add          string
+// 	Del          int
+// 	EditWork     string
+// 	ChangeBranch string
+// 	AssignBranch string
+// 	Toggle       int
+// 	List         bool
+// }
 
-func NewCmdFlags() *CmdFlags {
-	cf := CmdFlags{}
+// func NewCmdFlags() *CmdFlags {
+// 	cf := CmdFlags{}
 
-	flag.StringVar(&cf.Add, "add", "", "Add a new work specify title")
-	flag.StringVar(&cf.EditWork, "edit-work", "", "Edit a work by index & specify a new title. id:new_title")
-	flag.StringVar(&cf.ChangeBranch, "change-branch", "", "Change a assigned branch by index & specify a new title. id:new_title")
-	flag.StringVar(&cf.AssignBranch, "assign", "", "Assign the branch of the work by its id id:branch_name")
-	flag.IntVar(&cf.Del, "del", -1, "Specify work by index to delete")
-	flag.IntVar(&cf.Toggle, "toggle", -1, "Specify work by index to toggle complete true/false")
-	flag.BoolVar(&cf.List, "list", false, "List all works")
+// 	flag.StringVar(&cf.Add, "add", "", "Add a new work specify title")
+// 	flag.StringVar(&cf.EditWork, "edit-work", "", "Edit a work by index & specify a new title. id:new_title")
+// 	flag.StringVar(&cf.ChangeBranch, "change-branch", "", "Change a assigned branch by index & specify a new title. id:new_title")
+// 	flag.StringVar(&cf.AssignBranch, "assign", "", "Assign the branch of the work by its id id:branch_name")
+// 	flag.IntVar(&cf.Del, "del", -1, "Specify work by index to delete")
+// 	flag.IntVar(&cf.Toggle, "toggle", -1, "Specify work by index to toggle complete true/false")
+// 	flag.BoolVar(&cf.List, "list", false, "List all works")
 
-	flag.Parse()
+// 	flag.Parse()
 
-	return &cf
-}
+// 	return &cf
+// }
 
-func (cf *CmdFlags) Execute(works *Works) {
-	switch {
-	case cf.List:
-		works.print()
-	case cf.Add != "":
-		works.add(cf.Add)
-	case cf.EditWork != "":
-		parts := strings.SplitN(cf.EditWork, ":", 2)
-		if len(parts) != 2 {
-			fmt.Println("Error , invalid format for edit . Please id:new_title")
-			os.Exit(1)
-		}
-
-		index, err := strconv.Atoi(parts[0])
-
-		if err != nil {
-			fmt.Println("Error :invalid index for edit")
-			os.Exit(1)
-		}
-
-		works.editWork(index, parts[1])
-
-	case cf.ChangeBranch != "":
-		parts := strings.SplitN(cf.ChangeBranch, ":", 2)
-		if len(parts) != 2 {
-			fmt.Println("Error , invalid format for edit , Please choose id:new_branch_name")
-			os.Exit(1)
-		}
-
-		index, err := strconv.Atoi(parts[0])
-
-		if err != nil {
-			fmt.Println("Error:invalid index for edit")
-			os.Exit(1)
-		}
-
-		works.editBranch(index, parts[1])
-
-	case cf.AssignBranch != "":
-		parts := strings.SplitN(cf.AssignBranch, ":", 2)
-		if len(parts) != 2 {
-			fmt.Println("Error , invalid format for assigning branch . Please id:assigned_branch_name")
-			os.Exit(1)
-		}
-
-		index, err := strconv.Atoi(parts[0])
-
-		if err != nil {
-			fmt.Println("Error :invalid index for edit")
-			os.Exit(1)
-		}
-
-		works.assignWork(index, parts[1])
-
-	case cf.Toggle != -1:
-		works.toggle(cf.Toggle)
-
-	case cf.Del != -1:
-		works.delete(cf.Del)
-	default:
-		fmt.Println("Invalid command")
+func Execute(works *Works) error {
+	if len(os.Args) < 2 {
+		return fmt.Errorf("no command provided")
 	}
 
+	switch os.Args[1] {
+	case "add":
+		if len(os.Args) < 3 {
+			return fmt.Errorf("add command requires a task description")
+		}
+		works.add(strings.Join(os.Args[2:], " "))
+
+	case "list":
+		works.print()
+
+	case "edit-work":
+		if len(os.Args) < 4 {
+			return fmt.Errorf("edit-work requires index and new title")
+		}
+		index, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			return fmt.Errorf("invalid index")
+		}
+		works.editWork(index, strings.Join(os.Args[3:], " "))
+
+	case "change-branch":
+		if len(os.Args) < 4 {
+			return fmt.Errorf("change-branch requires index and new branch")
+		}
+		index, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			return fmt.Errorf("invalid index")
+		}
+		works.editBranch(index, os.Args[3])
+
+	case "assign":
+		if len(os.Args) < 4 {
+			return fmt.Errorf("assign requires index and branch name")
+		}
+		index, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			return fmt.Errorf("invalid index")
+		}
+		works.assignWork(index, os.Args[3])
+
+	case "toggle":
+		if len(os.Args) < 3 {
+			return fmt.Errorf("toggle requires task index")
+		}
+		index, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			return fmt.Errorf("invalid index")
+		}
+		works.toggle(index)
+
+	case "del":
+		if len(os.Args) < 3 {
+			return fmt.Errorf("del requires task index")
+		}
+		index, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			return fmt.Errorf("invalid index")
+		}
+		works.delete(index)
+
+	case "help":
+		displayHelpMessage()
+
+	default:
+		return fmt.Errorf("unknown command")
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -16,8 +16,13 @@ func main() {
 		os.Exit(0)
 	}
 
-	cmdFlags := NewCmdFlags()
-	cmdFlags.Execute(&works)
+	if err := Execute(&works); err != nil {
+		fmt.Println("Error:", err)
+		displayHelpMessage()
+		os.Exit(1)
+	}
+
+	// cmdFlags.Execute(&works)
 
 	storage.Save(works)
 }
@@ -38,33 +43,32 @@ Tasker CLI - Efficient Task Management
 =====================================================
 
 Commands:
-  -add         Add a new task
+  add         Add a new task
                Example: tasker -add "Complete project report"
 
-  -edit-work   Edit a task's title
+  edit-work   Edit a task's title
                Format: tasker -edit-work "id:new title"
                Example: tasker -edit-work "2:Updated task description"
 
-  -change-branch Modify task's branch assignment
+  change-branch Modify task's branch assignment
                  Format: tasker -change-branch "id:new branch"
                  Example: tasker -change-branch "1:Development"
 
-  -assign      Assign a branch to a specific task
+  assign      Assign a branch to a specific task
                Format: tasker -assign "id:branch_name"
                Example: tasker -assign "3:Backend"
 
-  -del         Delete a task by index
+  del         Delete a task by index
                Example: tasker -del 2
 
-  -toggle      Toggle task completion status
+  toggle      Toggle task completion status
                Example: tasker -toggle 1
 
-  -list        Display all tasks
+  list        Display all tasks
                Example: tasker -list
 
 Global Options:
   -h, -help    Show this help message
-  -version     Display application version
 
 For more information, visit:
 https://github.com/Sidontheway/Tasker_CLI
